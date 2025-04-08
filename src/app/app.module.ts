d0dad99887c9eb65a1a95be9409a8e7fdae321e2
Augusto-Rz <gutojruiz@gmail.com>
2025-04-08 17:17:52 -0300

    atualizando projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavComponent } from './component/template/nav/nav.component';
 
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { FooterComponent } from './component/template/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
